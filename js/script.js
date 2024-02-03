cb8392bc6d24abffe4fec5356f34415243518870
jahidulkanchan <mdj524156@gmail.com>
2024-02-03 09:09:58 +0600

    push

--- js/script.js
+++ js/script.js
@@ -4,3 +4,25 @@
 //   delay: 10,
 //   time: 1000
 // });
+
+// Slider Number1
+$(".slider1").owlCarousel({
+  loop: true,
+  margin: 30,
+  nav: true,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
